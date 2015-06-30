9906037d82de8592ef972ce923c5fd70db646a9d
Rajnish Panwar <rajnish_pawar90@yahoo.com>
2015-06-30 17:06:49 +0530

    nav

--- frontend/2.0/templates/landing/html_components/main_navbar.php
+++ frontend/2.0/templates/landing/html_components/main_navbar.php
@@ -32,220 +32,86 @@
             </li>
             <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
             <!--End Navigation toogle button-->
+            
+
+            
 
           </ul>
+          
+          <ul class="nav navbar-top-links pull-right">
 
-          <form class="mainnav-form" role="search">
-            <input type="text" class="form-control input-md mainnav-search-query" placeholder="Search">
-            <button class="btn btn-sm mainnav-form-btn"><i class="fa fa-search"></i></button>
-          </form>
 
-          <ul class="mainnav-menu">
-
-            <li class="dropdown ">
-              <a href="./index.html" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">
-              
-              Dashboards
-              <i class="mainnav-caret"></i>
+            <!--User dropdown-->
+            <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+            <li id="dropdown-user" class="dropdown">
+              <a href="#" data-toggle="dropdown" class="dropdown-toggle text-right">
+                <span class="pull-right">
+                  <img class="img-circle img-user media-object" src="img/av1.png" alt="Profile Picture">
+                </span>
+                <div class="username hidden-xs">John Doe</div>
               </a>
 
-              <ul class="dropdown-menu" role="menu">
-                <li>
-                  <a href="./index.html">
-                  <i class="fa fa-dashboard dropdown-icon "></i> 
-                  Analytics Dashboard
-                  </a>
-                </li>
 
-                <li>
-                  <a href="./dashboard-2.html">
-                  <i class="fa fa-dashboard dropdown-icon "></i> 
-                  Sidebar Dashboard
-                  </a>
-                </li>
+              <div class="dropdown-menu dropdown-menu-md dropdown-menu-right with-arrow panel-default">
 
-                <li>
-                  <a href="./dashboard-3.html">
-                  <i class="fa fa-dashboard dropdown-icon "></i> 
-                  Reports Dashboard
+                <!-- Dropdown heading  -->
+                <div class="pad-all bord-btm">
+                  <p class="text-lg text-muted text-thin mar-btm">750Gb of 1,000Gb Used</p>
+                  <div class="progress progress-sm">
+                    <div class="progress-bar" style="width: 70%;">
+                      <span class="sr-only">70%</span>
+                    </div>
+                  </div>
+                </div>
+
+
+                <!-- User dropdown menu -->
+                <ul class="head-list">
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-user fa-fw fa-lg"></i> Profile
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span class="badge badge-danger pull-right">9</span>
+                      <i class="fa fa-envelope fa-fw fa-lg"></i> Messages
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span class="label label-success pull-right">New</span>
+                      <i class="fa fa-gear fa-fw fa-lg"></i> Settings
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-question-circle fa-fw fa-lg"></i> Help
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-lock fa-fw fa-lg"></i> Lock screen
+                    </a>
+                  </li>
+                </ul>
+
+                <!-- Dropdown footer -->
+                <div class="pad-all text-right">
+                  <a href="pages-login.html" class="btn btn-primary">
+                    <i class="fa fa-sign-out fa-fw"></i> Logout
                   </a>
-                </li>
-              </ul>
+                </div>
+              </div>
             </li>
-
-
-
-            <li class="dropdown">
-
-              <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">
-                
-              Demo Pages
-              <i class="mainnav-caret"></i>
-              </a>
-
-              <ul class="dropdown-menu" role="menu">
-                <li>
-                  <a href="./page-pricing.html">
-                  <i class="fa fa-money dropdown-icon "></i> 
-                  Plans & Billing
-                  </a>
-                </li>
-
-                <li>
-                  <a href="./page-profile.html">
-                  <i class="fa fa-user dropdown-icon "></i> 
-                  Profile
-                  </a>
-                </li>
-
-                <li>
-                  <a href="./page-settings.html">
-                  <i class="fa fa-cogs dropdown-icon "></i> 
-                  Settings
-                  </a>
-                </li>
-
-                <li>
-                  <a href="./page-faq.html">
-                  <i class="fa fa-question dropdown-icon "></i> 
-                  FAQ
-                  </a>
-                </li>
-
-                <li>
-                  <a href="./pages-gallery.html">
-                  <i class="fa fa-picture-o dropdown-icon "></i> 
-                  Gallery
-                  </a>
-                </li>
-              </ul>
-            </li>          
-
-
-            <li class="dropdown ">
-
-              <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">
-                
-              Extras
-              <i class="mainnav-caret"></i>
-              </a>
-
-              <ul class="dropdown-menu" role="menu">
-                <li>
-                  <a href="./page-notifications.html">
-                  <i class="fa fa-bell dropdown-icon "></i> 
-                  Notifications
-                  </a>
-                </li>     
-
-                <li>
-                  <a href="./extras-icons.html">
-                  <i class="fa fa-smile-o dropdown-icon "></i> 
-                  Font Icons
-                  </a>
-                </li>   
-
-                <li>
-                  <a href="./page-blank.html">
-                  <i class="fa fa-file-o dropdown-icon "></i> 
-                  Page Blank
-                  </a>
-                </li> 
-
-                <li class="dropdown-submenu">
-                  <a tabindex="-1" href="#">
-                  <i class="fa fa-ban dropdown-icon "></i> 
-                  Error Pages
-                  </a>
-
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a href="./page-404.html">
-                      <i class="fa fa-ban dropdown-icon "></i> 
-                      404 Error
-                      </a>
-                    </li>
-
-                    <li>
-                      <a href="./page-500.html">
-                      <i class="fa fa-ban dropdown-icon "></i> 
-                      500 Error
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-
-                <li class="dropdown-submenu">
-
-                  <a tabindex="-1" href="#">
-                  <i class="fa fa-lock dropdown-icon "></i> 
-                  Login Pages
-                  </a>
-
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a href="./account-login.html">
-                      <i class="fa fa-unlock dropdown-icon "></i> 
-                      Login
-                      </a>
-                    </li>
-
-                    <li>
-                      <a href="./account-login-social.html">
-                      <i class="fa fa-unlock dropdown-icon "></i> 
-                      Login Social
-                      </a>
-                    </li>
-
-                    <li>
-                      <a href="./account-signup.html">
-                      <i class="fa fa-star dropdown-icon "></i> 
-                      Signup
-                      </a>
-                      </li>
-
-                    <li>
-                      <a href="./account-forgot.html">
-                      <i class="fa fa-envelope dropdown-icon "></i> 
-                      Forgot Password
-                      </a>
-                    </li>
-                  </ul>
-                </li> 
-
-              </ul>
-
-            </li>
-
-
-
-            <li class="dropdown">
-
-              <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">
-              More
-              <i class="mainnav-caret"></i>
-              </a>
-
-              <ul class="dropdown-menu" role="menu">
-                <li>
-                  <a href="../landing/">
-                  <i class="fa fa-external-link dropdown-icon "></i> 
-                  Landing Template
-                  </a>
-                </li>
-                <li>
-                  <a href="../launch/">
-                  <i class="fa fa-external-link dropdown-icon "></i> 
-                  Launch Template
-                  </a>
-                </li>
-              </ul>
-            </li>
+            <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+            <!--End user dropdown-->
 
           </ul>
         </div>
         <!--================================-->
         <!--End Navbar Dropdown-->
+
       
 
     </div> <!-- /.navbar-container -->
