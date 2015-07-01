2e926b1554fc6b273e1854f36af5c6e3b0519563
Rajnish Panwar <rajnish_pawar90@yahoo.com>
2015-07-01 15:39:29 +0530

    activity page

---
+++ frontend/2.0/templates/landing/activity.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+  <title>Collap &middot; Activity page</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <?php include_once 'html_components/header.php'; ?>
+
+</head>
+
+<body>
+
+  <div id="container" class="effect mainnav-lg">
+    
+    <?php include_once 'html_components/main_navbar.php'; ?>
+
+    <?php include_once 'html_components/sidebar_button.php'; ?>
+
+    <div id="content-container">
+
+
+      <div class="row col-md-offset-1 col-sm-offset-1 col-lg-offset-1 col-xs-offset-1">
+
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 ">
+
+          <div class="posts">
+
+            <div class="post">
+
+              <div class="post-aside" style="padding-top: 28px;">
+                <div class="post-date">
+                  <span class="post-date-day">09</span>
+                  <span class="post-date-month">Nov</span>
+                  <span class="post-date-year">2014</span>
+                </div>
+
+                <a href="#comments" class="post-comment">
+                  4
+                </a>
+              </div> <!-- /.post-aside -->
+
+              <div class="post-main">
+                <h3 class="post-title"><a href="#">Article Title: Collap V3 has been launched with new UX</a></h3>
+                <h4 class="post-meta">Published by <a href="javascript:;">Rajnish Panwar</a> in <a href="javascript:;">India</a></h4>
+
+                <img src="../../global/img/blog/blog-post-3.jpg" class="post-img img-responsive" alt="">
+
+                <div class="post-content">      
+                  
+                  <p>UX Myths collects the most frequent user experience misconceptions and explains why they don't hold true. And you don't have to take our word for it, we'll show you a lot of research findings and articles by design and usability gurus.</p>
+
+                  <p>User experience design (UXD or UED) is the process of enhancing user satisfaction by improving the usability, accessibility, and pleasure provided in the interaction between the user and the product.[1] User experience design encompasses traditional human–computer interaction (HCI) design, and extends it by addressing all aspects of a product or service as perceived by users.[2]</p>
+
+                </div> <!-- /.post-content -->
+
+              </div> <!-- /.post-main -->
+
+            </div> <!-- /.post --> 
+
+          </div> <!-- /.posts -->
+
+          <hr class="spacer-sm">
+
+          <a id="comments"></a>
+
+          <div class="heading-block">
+            <h3>Comments</h3>
+          </div>
+
+          <ol class="comment-list">
+
+            <li>
+              <div class="comment">
+
+                <div class="comment-avatar">
+                  <img alt="" src="../../global/img/avatars/avatar-4-md.jpg" class="avatar">
+                </div> <!-- /.comment-avatar -->
+
+                <div class="comment-meta">
+
+                  <span class="comment-author">
+                    <a href="javascript:;" class="url">Peter Landt</a>
+                  </span>
+
+                  <a href="javascript:;" class="comment-timestamp">
+                    April 3, 2013 at 6:47 am
+                  </a>
+
+                  -
+
+                  <a class="comment-reply-link" href="javascript:;">Reply</a>
+
+                </div> <!-- /.comment-meta -->
+
+                <div class="comment-body">
+                  <p>Nullam dictum felis eu pede mollis pretium.</p>
+                </div> <!-- /.comment-body -->
+
+              </div> <!-- /.comment -->
+            </li>
+
+            <li>
+              <div class="comment">
+
+                <div class="comment-avatar">
+                  <img alt="" src="../../global/img/avatars/avatar-3-md.jpg" class="avatar">
+                </div> <!-- /.comment-avatar -->
+
+                <div class="comment-meta">
+
+                  <span class="comment-author">
+                    <a href="javascript:;" class="url">Adelle Charles</a>
+                  </span>
+
+                  <a href="javascript:;" class="comment-timestamp">
+                    April 3, 2013 at 6:47 am
+                  </a>
+
+                  -
+
+                  <a class="comment-reply-link" href="javascript:;">Reply</a>
+
+                </div> <!-- /.comment-meta -->
+
+                <div class="comment-body">
+                  <p>Nullam dictum felis eu pede mollis pretium.</p>
+                </div> <!-- /.comment-body -->
+
+              </div> <!-- /.comment -->
+
+              <ol class="comment-list">
+                <li>
+                  <div class="comment">
+
+                    <div class="comment-avatar">
+                      <img alt="" src="../../global/img/avatars/avatar-5-md.jpg" class="avatar">
+                    </div> <!-- /.comment-avatar -->
+
+                    <div class="comment-meta">
+
+                      <span class="comment-author">
+                        <a href="javascript:;" class="url">Enda Nasution</a>
+                      </span>
+
+                      <a href="javascript:;" class="comment-timestamp">
+                        April 3, 2013 at 6:47 am
+                      </a>
+
+                      -
+
+                      <a class="comment-reply-link" href="javascript:;">Reply</a>
+
+                    </div> <!-- /.comment-meta -->
+
+                    <div class="comment-body">
+                      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
+                    </div> <!-- /.comment-body -->
+
+                  </div> <!-- /.comment -->                    
+                </li>
+
+                <li>
+                  <div class="comment">
+
+                    <div class="comment-avatar">
+                      <img alt="" src="../../global/img/avatars/avatar-5-md.jpg" class="avatar">
+                    </div> <!-- /.comment-avatar -->
+
+                    <div class="comment-meta">
+
+                      <span class="comment-author">
+                        <a href="javascript:;" class="url">Enda Nasution</a>
+                      </span>
+
+                      <a href="javascript:;" class="comment-timestamp">
+                        April 3, 2013 at 6:47 am
+                      </a>
+
+                      -
+
+                      <a class="comment-reply-link" href="javascript:;">Reply</a>
+
+                    </div> <!-- /.comment-meta -->
+
+                    <div class="comment-body">
+                      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
+                    </div> <!-- /.comment-body -->
+
+                  </div> <!-- /.comment -->  
+
+
+                  <ol class="comment-list">
+                    <li>
+                      <div class="comment">
+
+                        <div class="comment-avatar">
+                          <img alt="" src="../../global/img/avatars/avatar-5-md.jpg" class="avatar">
+                        </div> <!-- /.comment-avatar -->
+
+                        <div class="comment-meta">
+
+                          <span class="comment-author">
+                            <a href="javascript:;" class="url">Enda Nasution</a>
+                          </span>
+
+                          <a href="javascript:;" class="comment-timestamp">
+                            April 3, 2013 at 6:47 am
+                          </a>
+
+                          -
+
+                          <a class="comment-reply-link" href="javascript:;">Reply</a>
+
+                        </div> <!-- /.comment-meta -->
+
+                        <div class="comment-body">
+                          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
+                        </div> <!-- /.comment-body -->
+
+                      </div> <!-- /.comment -->                    
+                    </li>
+                  </ol>
+
+
+
+                </li>
+              </ol>
+            </li>
+
+          </ol>
+
+          <hr class="spacer-md">
+
+          <div class="heading-block">
+            <h3>Post a Comment</h3>
+          </div>
+
+          <form class="form form-horizontal">
+            <div class="form-group">
+              <div class="col-md-5 col-sm-12">
+                <label>Name: <span>*</span></label>
+                <input class="form-control" id="name" name="name" type="text" value="">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-5 col-sm-12">
+                <label>Email: <span>*</span></label>
+                <input class="form-control" type="email" id="email" name="email" value="">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-8 col-sm-12">
+                <label>Message: <span>*</span></label>
+                <textarea class="form-control" id="text" name="text" rows="6" cols="40"></textarea>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-8 col-sm-12">
+                <button class="btn btn-primary" type="submit">Submit Comment</button>
+              </div>
+            </div>
+
+           
+          </form>
+
+        </div> <!-- /.col -->
+
+
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-10">
+
+          <hr class="visible-xs">
+
+          <hr class="spacer-sm">
+          <hr class="spacer-xs">
+
+          <ul class="blog-stats">
+
+            <li class="" title="">
+              <a href="javascript:;">
+                <span class="blog-stats-mark">
+                  <i class="fa fa-twitter"></i>
+                </span>
+
+                <span class="blog-stats-count">1,245</span> 
+                <span class="blog-stats-label">Followers</span>
+              </a>
+            </li>
+
+            <li class="" title="">
+              <a href="javascript:;">
+                <span class="blog-stats-mark">
+                  <i class="fa fa-rss"></i>
+                </span>
+
+                <span class="blog-stats-count">2,390</span> 
+                <span class="blog-stats-label">Subscribers</span>
+              </a>
+            </li>
+
+            <li class="" title="">
+              <a href="javascript:;">
+                <span class="blog-stats-mark">
+                  <i class="fa fa-facebook"></i>
+                </span>
+
+                <span class="blog-stats-count">3,652</span> 
+                <span class="blog-stats-label">Likes</span>
+              </a>
+            </li>
+
+            <li class="" title="">
+              <a href="javascript:;">
+                <span class="blog-stats-mark">
+                  <i class="fa fa-google-plus"></i>
+                </span>
+
+                <span class="blog-stats-count">1,072</span> 
+                <span class="blog-stats-label">Subscribers</span>
+              </a>
+            </li>
+
+          </ul>
+
+          <hr class="spacer-sm">
+          <hr class="spacer-xs">
+
+          <h4>Most Popular Posts</h4>
+
+          <ul class="blog-popular-posts">
+            <li>
+              <div class="recent-post-thumbnail">
+                <a href="./page-blog-single.html">
+                  <img width="60" height="60" src="../../global/img/blog/blog-post-4-sm.jpg" alt="">
+                </a>
+              </div> <!-- /.recent-post-thumbnail -->
+
+              <div class="recent-post-title">
+                <h4>
+                  <a href="./page-blog-single.html"> 
+                    Collap new UX helps to understand better 
+                  </a>
+                </h4>
+
+                <span class="recent-post-meta">
+                  
+                </span> <!-- /.recent-post-meta -->
+              </div> <!-- /.recent-post-title -->
+
+            </li>
+
+            <li>
+              <div class="recent-post-thumbnail">
+                <a href="./page-blog-single.html">
+                  <img width="60" height="60" src="../../global/img/blog/blog-post-2-sm.jpg" alt="">
+                </a>
+              </div> <!-- /.recent-post-thumbnail -->
+
+              <div class="recent-post-title">
+                <h4>
+                  <a href="./page-blog-single.html"> 
+                    10 things must know about Startups
+                  </a>
+                </h4>
+
+                <span class="recent-post-meta">
+                  Finibus sed nec          
+                </span> <!-- /.recent-post-meta -->
+              </div> <!-- /.recent-post-title -->
+
+            </li>
+
+            <li>
+              <div class="recent-post-thumbnail">
+                <a href="./page-blog-single.html">
+                  <img width="60" height="60" src="../../global/img/blog/blog-post-3-sm.jpg" alt="">
+                </a>
+              </div> <!-- /.recent-post-thumbnail -->
+
+              <div class="recent-post-title">
+                <h4>
+                  <a href="./page-blog-single.html"> 
+                    Maecenas nec est pharetra, ornare leo 
+                  </a>
+                </h4>
+
+                <span class="recent-post-meta">
+                  Curabitur tincidunt           
+                </span> <!-- /.recent-post-meta -->
+              </div> <!-- /.recent-post-title -->
+
+            </li>
+          </ul>
+
+
+
+          <hr class="spacer-sm">
+          <hr class="spacer-xs">
+
+          <h4>Recent Posts</h4>
+
+          <ul class="fa-ul blog-ul">
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Sed ac arcu eu nunc efficitur volutpat et in arcu</a></li>
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Aliquam nec orci vestibulum tellus placerat iaculis sit amet quis tellus</a></li>
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Morbi imperdiet lorem eu efficitur rutrum</a></li>
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Vestibulum id neque quis nibh pulvinar sagittis</a></li>
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Maecenas nec est pharetra, ornare leo eget, pellentesque urna</a></li>
+          </ul>
+
+
+
+          <hr class="spacer-sm">
+          <hr class="spacer-xs">
+          
+          <h4>Categories</h4>
+
+          <ul class="fa-ul blog-ul">
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Duis ut justo</a></li>
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Curabitur tincidunt</a></li>
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Proin sit amet</a></li>
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Velit dictum fringilla</a></li>
+            <li><i class="fa-li fa fa-chevron-right"></i> <a href="#">Finibus sed nec</a></li>
+          </ul>
+
+        </div> <!-- /.col -->
+
+      </div> <!-- /.row -->
+ 
+    </div> <!-- id Content-container div end -->
+  
+  </div> <!-- id container div end -->
+
+
+  <?php include_once 'html_components/footer.php'; ?>
