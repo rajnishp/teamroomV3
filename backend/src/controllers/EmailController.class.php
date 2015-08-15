740086f7fd43bba006dad77efa1ec74b8f1040c5
rahul lahoria <rahullahoria@CAP-LAP-447>
2015-08-15 15:07:48 +0530

    email engine

--- backend/src/controllers/EmailController.class.php
+++ backend/src/controllers/EmailController.class.php
@@ -11,7 +11,86 @@ class EmailController extends BaseController  {
 	
 	}
 
-	
+	function startEmailCron(){
+		$this->pushUserNotifications();
+		$this->followUpOldUsers();
+		$this->inviteUser();
+
+	}
+
+	function pushUserNotifications(){
+
+		$userNotificationNoLogin24hours = $this-> notificationsDAO -> getNotificationNotLogin24Hurs();//by making join with user table
+
+		$emails = array();
+		foreach ($userNotificationNoLogin24hours as $key => $value) {
+			
+				$emails[$value->getUserEmailId()] .= $value->getNoticelUrl();
+			
+		}
+
+		$notificationEmail = $this-> genericEmailDAO -> getNotificationEmail();
+		foreach ($emails as $key => $value) {
+			$temp = $notificationEmail;
+			$this->sendMail( $key, $temp->getSubject(), str_replace("{{ notification }}", $value, $temp->getBody()));
+		}
+
+
+	}
+
+	function followUpOldUsers(){
+
+		$userNoNotificationNotLoginLast3days = $this -> userInfoDAO -> getUserNoNotificationNotLoginLast3days();
+
+		$emails = $this -> genericEmailDAO -> getFollowUpEmails();
+		
+		$usersAllReadyUnderFollowUp = $this -> userUnderFollowUpDAO -> getUserUnderFollowUp();
+
+
+		foreach ($userNoNotificationNotLoginLast3days as $key => $value) {
+
+			$this->sendMail(
+						$value->getEmailId(), 
+						str_replace("{{ username }}", $value->getUserFullName() , $emails[0]->getSubject()),
+						str_replace("{{ username }}", $value->getUserFullName() , $emails[0]->getBody()) 
+						);
+			$userUnderFollowUp = new UserUnderFollowUp($value->getUserId, $value->getEmailId(), 1, 0);
+			$this -> userUnderFollowUpDAO -> insert($userUnderFollowUp);
+			
+		}
+
+		foreach ($usersAllReadyUnderFollowUp as $key => $value) {
+			$this->sendMail(
+						$value->getEmailId(), 
+						str_replace("{{ username }}", $value->getUserFullName() , $emails[$value->getState()]->getSubject()),
+						str_replace("{{ username }}", $value->getUserFullName() , $emails[$value->getState()]->getBody()) 
+						);
+			$userUnderFollowUp = new UserUnderFollowUp($value->getUserId, $value->getEmailId(), $value->getState() + 1 , 0);
+			$this -> userUnderFollowUpDAO -> update($userUnderFollowUp);
+		}
+		
+
+
+
+	}
+
+	function inviteUser(){
+
+		$userToInvite = $this->invitaionDAO-> getUsersToInvite();
+
+		$emails = $this -> genericEmailDAO -> getInvitationEmails();
+
+		foreach ($userToInvite as $key => $value) {
+			$this->sendMail(
+						$value->getEmailId(), 
+						str_replace("{{ username }}", $value->getUserFullName() , $emails[$key]->getSubject()),
+						str_replace("{{ username }}", $value->getUserFullName() , $emails[$key]->getBody()) 
+						);
+		}
+
+	}
+
+
 	function sendMail($to, $subject, $message){
 
 		/*$to = "rahul_lahoria@yahoo.com";
